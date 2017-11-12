a23cc32d293652209326ddd41c9eb91529cf1dd1
Sensen <491469748@qq.com>
2017-11-12 22:16:42 +0800

    Push sourcecode to the repo

---
+++ pthread.c
@@ -0,0 +1,122 @@
+﻿/*
+  1. 一个buffer
+  2. buffer的锁
+  3. 通知机制,producer写好之后通知consumer 
+  4. 一个生产者线程
+  5.一个消费者线程
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <time.h>
+
+//buffer数据类型 可扩展
+typedef struct
+{
+    int num;
+    struct tm *ploc;
+}BUFFER;
+
+#define MAX_NUM 1000
+
+BUFFER buf;
+//lock
+pthread_mutex_t pc_mutex;
+//通知
+pthread_cond_t pc_condp, pc_condc;
+
+
+/*
+  生产者
+ */
+
+void * producer(void * nul)
+{
+    int i;
+    for (i = 1; i < MAX_NUM; ++i)
+    {
+        pthread_mutex_lock(&pc_mutex);
+
+        //等待条件变量
+        while(buf.num != 0)
+        {
+            pthread_cond_wait(&pc_condp, &pc_mutex);
+        }
+        //生产
+        buf.num = i;
+        time_t cur = time(NULL);
+        buf.ploc = localtime(&cur) ;
+        printf("producer produces %d ", buf.num );
+        printf("at %s\n", ctime(&cur));
+        //通知
+        pthread_cond_signal(&pc_condc);
+        pthread_mutex_unlock(&pc_mutex);
+    }
+    pthread_exit(NULL);
+
+}
+
+/*
+  消费者
+ */
+
+void * consumer(void * nul)
+{
+    int i;
+    for (i = 1; i < MAX_NUM; ++i)
+    {
+        pthread_mutex_lock(&pc_mutex);
+
+        //等待条件变量
+        while(buf.num == 0) {
+            pthread_cond_wait(&pc_condc, &pc_mutex);
+        }
+        //条件达到
+        time_t cur = time(NULL);
+        buf.ploc = localtime(&cur) ;
+        printf("consumer consumes %d ", buf.num);
+        printf("at %s\n", ctime(&cur));
+        buf.num = 0;
+        //通知consumer
+        pthread_cond_signal(&pc_condp);
+        pthread_mutex_unlock(&pc_mutex);
+    }
+
+    pthread_exit(NULL);
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    pthread_t thread[2];
+    pthread_attr_t attr;
+
+    buf.num = 0;
+
+    //锁和条件变量
+    pthread_mutex_init(&pc_mutex, NULL);
+    pthread_cond_init(&pc_condp, NULL);
+    pthread_cond_init(&pc_condc, NULL);
+
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    //producer
+    pthread_create(&thread[0], &attr, producer, NULL);
+    //consumer
+    pthread_create(&thread[1], &attr, consumer, NULL);
+
+    //连接线程
+    pthread_join(thread[0], NULL);
+    pthread_join(thread[1], NULL);
+
+    //清理资源
+    pthread_mutex_destroy(&pc_mutex);
+    pthread_cond_destroy(&pc_condc);
+    pthread_cond_destroy(&pc_condp);
+    pthread_attr_destroy(&attr);
+
+    pthread_exit(NULL);
+    return 0;
+}
---
+++ sema.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#define MAX 10 //队列长度
+ 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t full;     //填充的个数
+sem_t empty;    //空槽的个数
+ 
+int top = 0;   //队尾
+int bottom = 0; //队头
+ 
+int total = 0; //
+int amount = 0;
+void* produce(void* arg)
+{
+    int i;
+
+    for ( i = 0; i < MAX*2; i++)
+    {
+        //printf("producer is preparing data/n ");
+        sem_wait(&empty);//若空槽个数低于0阻塞
+    if(total == 50){
+      return (void*)1;
+    } 
+        //printf("emptyis %d\n", &empty);
+        pthread_mutex_lock(&mutex);
+        amount = amount+1;
+        //top = (top+1) % MAX;
+        printf("Stack amount is %d\n", amount);
+     
+        pthread_mutex_unlock(&mutex);
+        
+        sem_post(&full);
+        sleep(0.05);
+    }
+
+    return (void*)1;
+}
+ 
+void* consume(void* arg)
+{
+    int i;
+    for (;total < 50;)
+    {
+        //printf("consumer is preparing data ");
+        sem_wait(&full);//若填充个数低于0阻塞
+     
+        pthread_mutex_lock(&mutex);
+        amount = amount-1; 
+        total = total + 1;
+        //bottom = (bottom+1) % MAX;
+        printf("Stack amount is %d   ", amount);
+        printf("Already Consumed %d items\n", total);
+        pthread_mutex_unlock(&mutex);
+         
+        sem_post(&empty);
+sleep(0.01);
+    }
+ 
+    return (void*)2;
+}
+ 
+int main(int argc, char *argv[])
+{
+    pthread_t thid1;
+    pthread_t thid2;
+    pthread_t thid3;
+    pthread_t thid4;
+    pthread_t thid5;
+ 
+    int ret1;
+    int ret2;
+    int ret3;
+    int ret4;
+    int ret5;
+ 
+    sem_init(&full, 0, 0);
+    sem_init(&empty, 0, MAX);
+ 
+    pthread_create(&thid1, NULL, produce, NULL);
+    pthread_create(&thid2, NULL, consume, NULL);
+    pthread_create(&thid3, NULL, produce, NULL);
+    pthread_create(&thid4, NULL, consume, NULL);
+    pthread_create(&thid5, NULL, produce, NULL);
+
+    pthread_join(thid1, (void**)&ret1);
+    pthread_join(thid2, (void**)&ret2);
+    pthread_join(thid3, (void**)&ret3);
+    pthread_join(thid4, (void**)&ret4);
+    pthread_join(thid4, (void**)&ret5);
+ 
+    return 0;
+}
